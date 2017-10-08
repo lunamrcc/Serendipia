9bd8cd036d8771f0e5fbeb3321db6d365ef9266c
lunamrcc <A01209537@itesm.mx>
2017-10-07 23:33:54 -0500

    Statics corregidos

--- Index/views.py
+++ Index/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Index(request):
+	context = {}
+	return render(request, 'Index/dashboard_admin.html', context)
--- Serendipia/settings.py
+++ Serendipia/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Index',
+    'Index.apps.IndexConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'Serendipia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-	    ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
