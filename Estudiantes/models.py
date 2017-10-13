7af78827e50e4263a9a6303b77d5bc064cc08d9d
pacomonroy <pacomonroyu@hotmail.com>
2017-10-13 17:21:53 -0500

    "Estudiantes_0"

---
+++ Estudiantes/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+class Estudiantes(models.Model):
+
+    EstudianteID = models.IntegerField(
+        primary_key=True,
+        null=False,
+
+    )
+
+    Nombre = models.CharField(
+        max_length =50,
+        null=False,
+    )
+
+    aPaterno = models.CharField(
+        max_length=30,
+        null=False,
+    )
+    aMaterno = models.CharField(
+        max_length=30,
+        null=False,
+    )
+    Telefono = models.CharField(
+        max_length=15,
+        null=False,
+    )
+    Escuela = models.CharField(
+        max_length=50,
+        null=False,
+    )
+    Correo = models.EmailField(
+        null=False,
+    )
+    Activo = models.BooleanField(
+        default = True,
+    )
+
+    def __str__(self):
+        return self.Nombre +' '+ self.aPaterno +' '+ self.aMaterno
--- Serendipia/urls.py
+++ Serendipia/urls.py
@@ -17,5 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/',admin.site.urls),
     url(r'^administrador', include('Index.urls', namespace='administrador')),
+
+
 ]
